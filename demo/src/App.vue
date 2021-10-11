eeefc1781d437c78245cda66bac669325db13a27
GeniusLuo <505101092@qq.com>
2021-10-11 21:11:22 +0800

    demo

---
+++ demo/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      msg: 'hello1'
+    }
+  },
+  mounted() {
+    window.vue = this
+    console.log(this.msg)
+    console.log(this._data.msg)
+  }
+}
+</script>
+
+<style>
+
+</style>
